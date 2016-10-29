9de848b3202c5b5ea59ce4440964b86f6796390d
Yusuf Hoque <yusufhoque@gmail.com>
2016-10-29 22:12:09 +0100

    Enchancing the travis build script to include checks and further logs

--- script/cibuild.bash
+++ script/cibuild.bash
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 # Build the jeyll website
+echo "Executing -> bundle exec jekyll build"
 bundle exec jekyll build
+if [ $? -ne 0 ]
+then
+	echo "The jekyll build failed! Exiting"
+fi
 
 # Run a html test check
+echo "Executing -> bundle exec htmlproofer ./_site/ --url-ignore \"/#/\""
 bundle exec htmlproofer ./_site/ --url-ignore "/#/"
+if [ $? -ne 0 ]
+then
+        echo "The HTML Check failed! Exiting"
+fi
+
